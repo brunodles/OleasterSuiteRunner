fce4bb6660ca95b4f7b670f5d1f8a582bb00341c
Bruno de Lima <dlimaun@gmail.com>
2017-04-01 10:33:58 -0300

    Add StaticRunnerHelper with some useful methods

---
+++ src/main/java/com/brunodles/oleaster/StaticRunnerHelper.java
@@ -0,0 +1,52 @@
+package com.brunodles.oleaster;
+
+import com.mscharhag.oleaster.runner.Invokable;
+
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.describe;
+
+@SuppressWarnings("WeakerAccess")
+public final class StaticRunnerHelper {
+
+    private StaticRunnerHelper() {
+    }
+
+    public static void given(Class aClass, Invokable block) {
+        given(aClass.getSimpleName(), block);
+    }
+
+    public static void given(String text, Invokable block) {
+        describe("Given " + text, block);
+    }
+
+    public static void context(String text, Invokable block) {
+        describe("Context " + text, block);
+    }
+
+    public static void when(String text, Invokable block) {
+        describe("When " + text, block);
+    }
+
+    public static void on(String text, Invokable block) {
+        describe("On " + text, block);
+    }
+
+    public static void with(String text, Invokable block) {
+        describe("With " + text, block);
+    }
+
+    public static void xgiven(Class aClass, Invokable block) {}
+
+    public static void xgiven(String text, Invokable block) {}
+
+    public static void xcontext(String text, Invokable block) {}
+
+    public static void xwhen(String text, Invokable block) {}
+
+    public static void xon(String text, Invokable block) {}
+
+    public static void xwith(String text, Invokable block) {}
+
+    public static void xdescribe(String text, Invokable block) {}
+
+    public static void xit(String text, Invokable block) {}
+}
---
+++ src/test/java/com/brunodles/oleaster/suiterunner/StaticRunnerHelperTest.java
@@ -0,0 +1,73 @@
+package com.brunodles.oleaster.suiterunner;
+
+import com.brunodles.oleaster.StaticRunnerHelper;
+import com.brunodles.oleaster.suiterunner.OleasterSuiteRunner;
+import com.mscharhag.oleaster.runner.suite.Suite;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static com.brunodles.oleaster.StaticRunnerHelper.*;
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(OleasterSuiteRunner.class)
+public class StaticRunnerHelperTest {
+
+    private OleasterSuiteRunner runner;
+
+    private List<Suite> children;
+
+    {
+        given(OleasterSuiteRunner.class, () -> {
+            beforeEach(() -> {
+                runner = new OleasterSuiteRunner(TestClass.class);
+            });
+            when("getChildren", () -> {
+                beforeEach(() -> {
+                    children = runner.getChildren();
+                });
+                it("should return a list with 6 items", () -> {
+                    assertEquals(6, children.size());
+                });
+                itContains("Given String");
+                itContains("Given a object");
+                itContains("Context do something");
+                itContains("When do something");
+                itContains("On do something");
+                itContains("With a parameter");
+            });
+        });
+    }
+
+    private void itContains(String s) {
+        it("should contain a block with '" + s + "'", () -> contains(children, s));
+    }
+
+    private static void contains(List<Suite> children, String description) {
+        for (Suite child : children)
+            if (description.equals(child.getDescription()))
+                return;
+
+        fail("Didn't found a suite with description '" + description + "'");
+    }
+
+    public static class TestClass {
+        {
+            given(String.class, () -> {});
+            given("a object", () -> {});
+            context("do something", () -> {});
+            when("do something", () -> {});
+            on("do something", () -> {});
+            with("a parameter", () -> {});
+            xgiven(Boolean.class, () -> {});
+            xgiven("Another object", () -> {});
+            xcontext("do something", () -> {});
+            xwhen("do something", () -> {});
+            xon("do something", () -> {});
+            xwith("a parameter", () -> {});
+        }
+    }
+}
